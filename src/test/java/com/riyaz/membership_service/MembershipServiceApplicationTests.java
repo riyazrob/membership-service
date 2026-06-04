8330be1d8e8e928e740352bfe6a71f1f9d246310
Riyaz Mohammad <riyazrob3@gmail.com>
2026-06-04 09:57:28 -0500

    Added Spring Boot project

---
+++ src/test/java/com/riyaz/membership_service/MembershipServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riyaz.membership_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
