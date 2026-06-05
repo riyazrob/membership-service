9d254d6af3eda02fda21b4d73779dcd5e041690e
Riyaz Mohammad <riyazrob3@gmail.com>
2026-06-04 20:00:11 -0500

    API integrations

--- src/main/java/com/riyaz/membership_service/controller/MemberController.java
+++ src/main/java/com/riyaz/membership_service/controller/MemberController.java
@@ -2,8 +2,7 @@ package com.riyaz.membership_service.controller;
 
 import com.riyaz.membership_service.model.Member;
 import com.riyaz.membership_service.service.MemberService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +18,16 @@ public class MemberController {
     @GetMapping("/members")
     public List<Member> getMembers() {
         return memberService.getMembers();
+    }
+
+    @PostMapping("/members")
+    public Member addMember(@RequestBody Member member) {
+        return memberService.addMember(member);
+    }
+
+    @DeleteMapping("/members/{id}")
+    public String deleteMember(@PathVariable Long id) {
+        memberService.deleteMember(id);
+        return "Member deleted successfully";
     }
 }
--- src/main/java/com/riyaz/membership_service/service/MemberService.java
+++ src/main/java/com/riyaz/membership_service/service/MemberService.java
@@ -3,15 +3,30 @@ package com.riyaz.membership_service.service;
 import com.riyaz.membership_service.model.Member;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class MemberService {
 
+    private final List<Member> members = new ArrayList<>();
+
+    public MemberService() {
+        members.add(new Member(1L, "Riyaz", "riyaz@email.com"));
+        members.add(new Member(2L, "John", "john@email.com"));
+    }
+
     public List<Member> getMembers() {
-        return List.of(
-                new Member(1L, "Riyaz", "riyaz@email.com"),
-                new Member(2L, "John", "john@email.com")
-        );
+        return members;
+    }
+
+    public Member addMember(Member member) {
+        members.add(member);
+        return member;
+    }
+
+    public void deleteMember(Long id) {
+        members.removeIf(member -> member.getId().equals(id));
     }
 }
+``
